131c7174370466e3fb5328d7558d31552a376c57
sanakm <skass094@gmail.com>
2016-04-22 18:03:04 +0000

    fixed validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,31 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+
   belongs_to :service
   belongs_to :size
   has_many :orders
   has_many :comments
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true
+  validates :password_digest, presence: true, length: {minimum:6}
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :postalcode, presence: true
+
+  validates :postalcode, format: { with: /\A[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d\z/,
+    message: "is not a valid postal code"}
+
+  validates :email, format: { with: /\A[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}\z/, 
+    message: "this is not a valid email address"}
+
+  validates :phone, format: { with: /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/, 
+    message: "this is not a valid phone number"}
+
+  validates :name, format: { with: /\A[\\p{L} .'-]+\z}/,
+    message: "this is not a name"}
+
+  # use this if above format doesn't work: validates_format_of :postalcode, with: /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/, :message => "is not a valid postal code"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420191659) do
+ActiveRecord::Schema.define(version: 20160422153857) do
 
   create_table "comments", force: :cascade do |t|
     t.text    "description"
@@ -32,10 +32,10 @@ ActiveRecord::Schema.define(version: 20160420191659) do
 
   create_table "services", force: :cascade do |t|
     t.string   "name"
-    t.string   "socks_per_month"
+    t.integer  "socks_per_month"
     t.integer  "cost"
-    t.datetime "service_start"
-    t.date     "service_end"
+    t.datetime "created_at"
+    t.date     "updated_at"
   end
 
   create_table "sizes", force: :cascade do |t|
@@ -43,11 +43,11 @@ ActiveRecord::Schema.define(version: 20160420191659) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.boolean  "account_type", default: false
+    t.boolean  "account_type",    default: false
     t.string   "name"
     t.string   "email"
     t.string   "phone"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "address"
     t.string   "city"
     t.string   "postalcode"
