ae1a642dfae16a93107dec490bc6a199a433b0a0
Brian Bancroft <=>
2016-04-23 21:20:48 +0000

    some rando

--- public/javascript/sidebar_links.js
+++ public/javascript/sidebar_links.js
@@ -1,130 +1,186 @@
+function initPage(){
+  $("#post_complement").hide();
+  $("#post_complaint").hide();
+  $("#view_past_orders").hide();
+  $("#edit_order").hide();
+  $("#edit_shipping").hide();
+  $("#edit_profile").hide();
+  $("#view_next_order").hide();
+  $("#vieworder_hist").hide();
+  $("#view_action_complaints").hide();
+  $("#view_approve_compliments").hide();
+}
+
+function showCustomerLanding() {
+  $("#post_complement").hide();
+  $("#post_complaint").hide();
+  $("#view_past_orders").hide();
+  $("#edit_order").hide();
+  $("#edit_shipping").hide();
+  $("#edit_profile").hide();
+  $("#default_stuff").show();              // The function returns the product of p1 and p2
+}
+
+function showProfile(){
+  $("#post_complement").hide();
+  $("#post_complaint").hide();
+  $("#view_past_orders").hide();
+  $("#edit_order").hide();
+  $("#edit_shipping").hide();
+  $("#edit_profile").show();
+  $("#default_stuff").hide();
+}
+
+function showEditShipping(){
+  $("#post_complement").hide();
+  $("#post_complaint").hide();
+  $("#view_past_orders").hide();
+  $("#edit_order").hide();
+  $("#edit_shipping").show();
+  $("#edit_profile").hide();
+  $("#default_stuff").hide();
+}
+
+function showEditOrder(){
+  $("#post_complement").hide();
+  $("#post_complaint").hide();
+  $("#view_past_orders").hide();
+  $("#edit_order").show();
+  $("#edit_shipping").hide();
+  $("#edit_profile").hide();
+  $("#default_stuff").hide();
+}
+function showViewOrderHistory(){
+  $("#post_complement").hide();
+  $("#post_complaint").hide();
+  $("#view_past_orders").show();
+  $("#edit_order").hide();
+  $("#edit_shipping").hide();
+  $("#edit_profile").hide();
+  $("#default_stuff").hide();
+}
+
+function showMakeAComplaint(){
+  $("#post_complement").hide();
+  $("#post_complaint").show();
+  $("#view_past_orders").hide();
+  $("#edit_order").hide();
+  $("#edit_shipping").hide();
+  $("#edit_profile").hide();
+  $("#default_stuff").hide();
+}
+
+function showMakeACompliment(){
+  $("#post_complement").show();
+  $("#post_complaint").hide();
+  $("#view_past_orders").hide();
+  $("#edit_order").hide();
+  $("#edit_shipping").hide();
+  $("#edit_profile").hide();
+  $("#default_stuff").hide();
+}
+
+function showEmployeeLanding(){
+  $("#default_view").show();
+  $("#view_next_order").hide();
+  $("#vieworder_hist").hide();
+  $("#view_action_complaints").hide();
+  $("#view_approve_compliments").hide();
+}
+
+function showUpcomingOrder(){
+  $("#default_view").hide();
+  $("#view_next_order").show();
+  $("#vieworder_hist").hide();
+  $("#view_action_complaints").hide();
+  $("#view_approve_compliments").hide();
+}
+
+function showAllPastOrders(){
+  $("#default_view").hide();
+  $("#view_next_order").hide();
+  $("#vieworder_hist").show();
+  $("#view_action_complaints").hide();
+  $("#view_approve_compliments").hide();
+}
+
+function showAndActionComplaints(){
+  $("#default_view").hide();
+  $("#view_next_order").hide();
+  $("#vieworder_hist").hide();
+  $("#view_action_complaints").show();
+  $("#view_approve_compliments").hide();
+}
+function showAndApproveCompliments(){
+  $("#default_view").hide();
+  $("#view_next_order").hide();
+  $("#vieworder_hist").hide();
+  $("#view_action_complaints").hide();
+  $("#view_approve_compliments").show();
+}
+
+
+
 $(document).ready(function(){
-        $("#post_complement").hide();
-        $("#post_complaint").hide();
-        $("#view_past_orders").hide();
-        $("#edit_order").hide();
-        $("#edit_shipping").hide();
-        $("#edit_profile").hide();
-        $("#view_next_order").hide();
-        $("#vieworder_hist").hide();
-        $("#view_action_complaints").hide();
-        $("#view_approve_compliments").hide();
+  var bookmark = <%= session[:page_bookmark] %>;
+  switch(expression) {
+    case "hugs":
+        showEditShipping();
+        break;
+    default:
+        initPage();
+}
+  initPage();
     });
 
 // CUSTOMER PAGE
 
   $("#main_page").click(function(){
-        $("#post_complement").hide();
-        $("#post_complaint").hide();
-        $("#view_past_orders").hide();
-        $("#edit_order").hide();
-        $("#edit_shipping").hide();
-        $("#edit_profile").hide();
-        $("#default_stuff").show();
+      showCustomerLanding();
     });
 
     $("#link_profile").click(function(){
-        $("#post_complement").hide();
-        $("#post_complaint").hide();
-        $("#view_past_orders").hide();
-        $("#edit_order").hide();
-        $("#edit_shipping").hide();
-        $("#edit_profile").show();
-        $("#default_stuff").hide();
+      showProfile();
     });
 
     $("#edship").click(function(){
-        $("#post_complement").hide();
-        $("#post_complaint").hide();
-        $("#view_past_orders").hide();
-        $("#edit_order").hide();
-        $("#edit_shipping").show();
-        $("#edit_profile").hide();
-        $("#default_stuff").hide();
+      showEditShipping();
     });
     $("#edorder").click(function(){
-        $("#post_complement").hide();
-        $("#post_complaint").hide();
-        $("#view_past_orders").hide();
-        $("#edit_order").show();
-        $("#edit_shipping").hide();
-        $("#edit_profile").hide();
-        $("#default_stuff").hide();
+      showEditOrder();
     });
     $("#viewhist").click(function(){
-        $("#post_complement").hide();
-        $("#post_complaint").hide();
-        $("#view_past_orders").show();
-        $("#edit_order").hide();
-        $("#edit_shipping").hide();
-        $("#edit_profile").hide();
-        $("#default_stuff").hide();
+      showViewOrderHistory();
     });
     $("#sosorry").click(function(){
-        $("#post_complement").hide();
-        $("#post_complaint").show();
-        $("#view_past_orders").hide();
-        $("#edit_order").hide();
-        $("#edit_shipping").hide();
-        $("#edit_profile").hide();
-        $("#default_stuff").hide();
+      showMakeAComplaint();
     });
     $("#attaboy").click(function(){
-        $("#post_complement").show();
-        $("#post_complaint").hide();
-        $("#view_past_orders").hide();
-        $("#edit_order").hide();
-        $("#edit_shipping").hide();
-        $("#edit_profile").hide();
-        $("#default_stuff").hide();
+      showMakeACompliment();
     });
 
 
 //EMPLOYEE page
 
     $("#employee_welcome").click(function(){
-      $("#default_view").show();
-      $("#view_next_order").hide();
-      $("#vieworder_hist").hide();
-      $("#view_action_complaints").hide();
-      $("#view_approve_compliments").hide();
+      showEmployeeLanding();
     });
 
     $("#vieworder").click(function(){
-      $("#default_view").hide();
-      $("#view_next_order").show();
-      $("#vieworder_hist").hide();
-      $("#view_action_complaints").hide();
-      $("#view_approve_compliments").hide();
+      showUpcomingOrder();
     });
 
     $("#viewhist").click(function(){
-      $("#default_view").hide();
-      $("#view_next_order").hide();
-      $("#vieworder_hist").show();
-      $("#view_action_complaints").hide();
-      $("#view_approve_compliments").hide();
+      showAllPastOrders();
     });
 
     $("#sosorry_admin").click(function(){
-      $("#default_view").hide();
-      $("#view_next_order").hide();
-      $("#vieworder_hist").hide();
-      $("#view_action_complaints").show();
-      $("#view_approve_compliments").hide();
+      showAndActionComplaints();
     });
 
     $("#attaboy_admin").click(function(){
-      $("#default_view").hide();
-      $("#view_next_order").hide();
-      $("#vieworder_hist").hide();
-      $("#view_action_complaints").hide();
-      $("#view_approve_compliments").show();
+      showAndApproveCompliments();
     });
-
-
-
-
 
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
